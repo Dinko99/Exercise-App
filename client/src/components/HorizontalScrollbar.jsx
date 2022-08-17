3147acf15c41097bf4d4b14f40507df62dd1e2c8
Aleksandar Dinic <dinkodin99@gmail.com>
2022-08-17 14:06:38 +0200

    Downloaded icons for body parts and added data for icons

--- client/src/components/HorizontalScrollbar.jsx
+++ client/src/components/HorizontalScrollbar.jsx
@@ -1,14 +1,29 @@
 import './HorizontalScrollbar.scss'
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
+import Icons from '../utils/bodyPartIcons'
+import { useState } from 'react'
+
 
 const HorizontalScrollbar = ({data}) => {
+  const [icons,setIcons] = useState(Icons)
+
   return (
     <div className='horizontal-scrollbar'>
+      <div className="icons-container">
+      {
+        icons.map((icon)=>{
+          return <img src={icon.image} alt='image' key={icon.id} />
+        })
+      }
+      </div>
+      <div className="body-part-title-container">
         {
-            data.map((bodyPart) => {
-                return <h1>{bodyPart}</h1>
+            data.map((item) => {
+                return <h1 key={item.id}>{item}</h1>
             })
         }
+      </div>
+        
     </div>
   )
 }
---
+++ client/src/utils/bodyPartIcons.js
@@ -0,0 +1,60 @@
+import All from '../assets/icons/bodyPartIcons/all.png'
+import Back from '../assets/icons/bodyPartIcons/back.png'
+import Cardio from '../assets/icons/bodyPartIcons/cardio.png'
+import Chest from '../assets/icons/bodyPartIcons/chest.png'
+import LowerArm from '../assets/icons/bodyPartIcons/lower-arm.png'
+import LowerLeg from '../assets/icons/bodyPartIcons/lower-leg.png'
+import Neck from '../assets/icons/bodyPartIcons/neck.png'
+import Shoulders from '../assets/icons/bodyPartIcons/shoulders.png'
+import UpperArm from '../assets/icons/bodyPartIcons/upper-arm.png'
+import UpperLeg from '../assets/icons/bodyPartIcons/upper-leg.png'
+import Waist from '../assets/icons/bodyPartIcons/waist.png'
+
+const bodyPartIcons = [
+    {
+        id: 1,
+        image: All,
+    },
+    {
+        id: 2,
+        image: Back,
+    },
+    {
+        id: 3,
+        image: Cardio,
+    },
+    {
+        id: 4,
+        image: Chest,
+    },
+    {
+        id: 5,
+        image: LowerArm,
+    },
+    {
+        id: 6,
+        image: LowerLeg,
+    },
+    {
+        id: 7,
+        image: Neck,
+    },
+    {
+        id: 8,
+        image: Shoulders,
+    },
+    {
+        id: 9,
+        image: UpperArm,
+    },
+    {
+        id: 10,
+        image: UpperLeg,
+    },
+    {
+        id: 11,
+        image: Waist,
+    },
+]
+
+export default bodyPartIcons;
