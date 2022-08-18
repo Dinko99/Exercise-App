b135594fb76e932e460568393e8b7711688de898
Aleksandar Dinic <dinkodin99@gmail.com>
2022-08-19 00:38:27 +0200

    Installed react-icons, Finished horizontal scrollbar menu

--- client/src/components/BodyPart.jsx
+++ client/src/components/BodyPart.jsx
@@ -1,12 +1,15 @@
 import Icons from '../utils/bodyPartIcons'
 import { useState } from 'react'
 
-const BodyPart = ({index,item}) => {
+
+const BodyPart = ({index,item,setBodyPart,bodyPart}) => {
     const [icons,setIcons] = useState(Icons)
 
   return (
-    <div className="icon-card">
-        <img src={icons[index].image} alt={item} />
+    <div className="icon-card" onClick={()=> setBodyPart(item)}>
+        <img src={icons[index].image} alt={item} style={{
+      borderTop: bodyPart === item ? '2px solid #263EEA' : 'none'
+    }}  />
         <h1>{item}</h1>
     </div>
     
--- client/src/components/HorizontalScrollbar.jsx
+++ client/src/components/HorizontalScrollbar.jsx
@@ -1,19 +1,39 @@
 import './HorizontalScrollbar.scss'
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
-
+import { useContext } from 'react'
+import { FaArrowLeft,FaArrowRight } from 'react-icons/fa'
 import BodyPart from './BodyPart'
+
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <FaArrowLeft onClick={()=> scrollPrev()} className='arrow-left' />
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <FaArrowRight onClick={()=> scrollNext()} className='arrow-right' />
+  );
+};
 
 
 const HorizontalScrollbar = ({data, setBodyPart, bodyParts, bodyPart}) => {
 
   return (
-    <ScrollMenu>
+    <div className="horizontal-scrollbar">
+    <ScrollMenu LeftArrow={LeftArrow}  RightArrow={RightArrow}>
       {
         data.map((item,index)=>{
-          return <BodyPart key={item.id || item} item={item} index={index} />
+          return <BodyPart key={item.id || item} item={item} index={index} setBodyPart={setBodyPart} bodyPart={bodyPart} />
         })
       }
     </ScrollMenu>
+    </div>
     
   )
 }
